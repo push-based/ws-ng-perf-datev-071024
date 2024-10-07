937fe3fb479df180df05d6257cb081b90ef4aeda
Julian Jandl <julian@jandl.wien>
2024-10-07 14:17:48 +0200

    feat: implement zone-optimizations

--- src/app/shared/cdk/element-visibility/element-visibility.directive.ts
+++ src/app/shared/cdk/element-visibility/element-visibility.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, inject, output } from '@angular/core';
+import { Directive, ElementRef, inject, NgZone, output } from '@angular/core';
 import { filter, fromEvent, map } from 'rxjs';
 
 @Directive({
@@ -8,23 +8,29 @@ import { filter, fromEvent, map } from 'rxjs';
 export class ElementVisibilityDirective {
   private elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
 
+  private zone = inject(NgZone);
+
   elementVisible = output();
 
   constructor() {
-    fromEvent(document, 'scroll')
-      .pipe(
-        filter(() => !!document.scrollingElement),
-        map(() => {
-          const { scrollTop, clientHeight } = document.scrollingElement!;
-          return (
-            scrollTop + clientHeight + 100 >=
-            this.elementRef.nativeElement.offsetTop
-          );
-        }),
-        filter(Boolean),
-      )
-      .subscribe(() => {
-        this.elementVisible.emit();
-      });
+    this.zone.runOutsideAngular(() => {
+      fromEvent(document, 'scroll')
+        .pipe(
+          filter(() => !!document.scrollingElement),
+          map(() => {
+            const { scrollTop, clientHeight } = document.scrollingElement!;
+            return (
+              scrollTop + clientHeight + 100 >=
+              this.elementRef.nativeElement.offsetTop
+            );
+          }),
+          filter(Boolean),
+        )
+        .subscribe(() => {
+          this.zone.run(() => {
+            this.elementVisible.emit();
+          });
+        });
+    });
   }
 }
